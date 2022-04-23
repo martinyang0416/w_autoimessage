0374014aa309276dad0932cb7bf75cbd8471ddf1
Yuhao Yang <martin_yang0416@hotmail.com>
2022-04-23 15:18:21 -0400

    init

---
+++ main.py
@@ -0,0 +1,223 @@
+import time
+import random
+import schedule
+import subprocess
+from datetime import datetime
+import random
+import requests, json
+
+# global variables:
+phone_number = "+1" # please enter the phone number here
+information_prep_time = "10:16" # just set information_prep_time, scheduled_time to be the same time
+scheduled_time = "10:16"
+
+global_list = None
+
+
+def prepare_message():
+    cnames = {
+        'aliceblue': '#F0F8FF',
+        'antiquewhite': '#FAEBD7',
+        'aqua': '#00FFFF',
+        'aquamarine': '#7FFFD4',
+        'azure': '#F0FFFF',
+        'beige': '#F5F5DC',
+        'bisque': '#FFE4C4',
+        'black': '#000000',
+        'blanchedalmond': '#FFEBCD',
+        'blue': '#0000FF',
+        'blueviolet': '#8A2BE2',
+        'brown': '#A52A2A',
+        'burlywood': '#DEB887',
+        'cadetblue': '#5F9EA0',
+        'chartreuse': '#7FFF00',
+        'chocolate': '#D2691E',
+        'coral': '#FF7F50',
+        'cornflowerblue': '#6495ED',
+        'cornsilk': '#FFF8DC',
+        'crimson': '#DC143C',
+        'cyan': '#00FFFF',
+        'darkblue': '#00008B',
+        'darkcyan': '#008B8B',
+        'darkgoldenrod': '#B8860B',
+        'darkgray': '#A9A9A9',
+        'darkgreen': '#006400',
+        'darkkhaki': '#BDB76B',
+        'darkmagenta': '#8B008B',
+        'darkolivegreen': '#556B2F',
+        'darkorange': '#FF8C00',
+        'darkorchid': '#9932CC',
+        'darkred': '#8B0000',
+        'darksalmon': '#E9967A',
+        'darkseagreen': '#8FBC8F',
+        'darkslateblue': '#483D8B',
+        'darkslategray': '#2F4F4F',
+        'darkturquoise': '#00CED1',
+        'darkviolet': '#9400D3',
+        'deeppink': '#FF1493',
+        'deepskyblue': '#00BFFF',
+        'dimgray': '#696969',
+        'dodgerblue': '#1E90FF',
+        'firebrick': '#B22222',
+        'floralwhite': '#FFFAF0',
+        'forestgreen': '#228B22',
+        'fuchsia': '#FF00FF',
+        'gainsboro': '#DCDCDC',
+        'ghostwhite': '#F8F8FF',
+        'gold': '#FFD700',
+        'goldenrod': '#DAA520',
+        'gray': '#808080',
+        'green': '#008000',
+        'greenyellow': '#ADFF2F',
+        'honeydew': '#F0FFF0',
+        'hotpink': '#FF69B4',
+        'indianred': '#CD5C5C',
+        'indigo': '#4B0082',
+        'ivory': '#FFFFF0',
+        'khaki': '#F0E68C',
+        'lavender': '#E6E6FA',
+        'lavenderblush': '#FFF0F5',
+        'lawngreen': '#7CFC00',
+        'lemonchiffon': '#FFFACD',
+        'lightblue': '#ADD8E6',
+        'lightcoral': '#F08080',
+        'lightcyan': '#E0FFFF',
+        'lightgoldenrodyellow': '#FAFAD2',
+        'lightgreen': '#90EE90',
+        'lightgray': '#D3D3D3',
+        'lightpink': '#FFB6C1',
+        'lightsalmon': '#FFA07A',
+        'lightseagreen': '#20B2AA',
+        'lightskyblue': '#87CEFA',
+        'lightslategray': '#778899',
+        'lightsteelblue': '#B0C4DE',
+        'lightyellow': '#FFFFE0',
+        'lime': '#00FF00',
+        'limegreen': '#32CD32',
+        'linen': '#FAF0E6',
+        'magenta': '#FF00FF',
+        'maroon': '#800000',
+        'mediumaquamarine': '#66CDAA',
+        'mediumblue': '#0000CD',
+        'mediumorchid': '#BA55D3',
+        'mediumpurple': '#9370DB',
+        'mediumseagreen': '#3CB371',
+        'mediumslateblue': '#7B68EE',
+        'mediumspringgreen': '#00FA9A',
+        'mediumturquoise': '#48D1CC',
+        'mediumvioletred': '#C71585',
+        'midnightblue': '#191970',
+        'mintcream': '#F5FFFA',
+        'mistyrose': '#FFE4E1',
+        'moccasin': '#FFE4B5',
+        'navajowhite': '#FFDEAD',
+        'navy': '#000080',
+        'oldlace': '#FDF5E6',
+        'olive': '#808000',
+        'olivedrab': '#6B8E23',
+        'orange': '#FFA500',
+        'orangered': '#FF4500',
+        'orchid': '#DA70D6',
+        'palegoldenrod': '#EEE8AA',
+        'palegreen': '#98FB98',
+        'paleturquoise': '#AFEEEE',
+        'palevioletred': '#DB7093',
+        'papayawhip': '#FFEFD5',
+        'peachpuff': '#FFDAB9',
+        'peru': '#CD853F',
+        'pink': '#FFC0CB',
+        'plum': '#DDA0DD',
+        'powderblue': '#B0E0E6',
+        'purple': '#800080',
+        'red': '#FF0000',
+        'rosybrown': '#BC8F8F',
+        'royalblue': '#4169E1',
+        'saddlebrown': '#8B4513',
+        'salmon': '#FA8072',
+        'sandybrown': '#FAA460',
+        'seagreen': '#2E8B57',
+        'seashell': '#FFF5EE',
+        'sienna': '#A0522D',
+        'silver': '#C0C0C0',
+        'skyblue': '#87CEEB',
+        'slateblue': '#6A5ACD',
+        'slategray': '#708090',
+        'snow': '#FFFAFA',
+        'springgreen': '#00FF7F',
+        'steelblue': '#4682B4',
+        'tan': '#D2B48C',
+        'teal': '#008080',
+        'thistle': '#D8BFD8',
+        'tomato': '#FF6347',
+        'turquoise': '#40E0D0',
+        'violet': '#EE82EE',
+        'wheat': '#F5DEB3',
+        'white': '#FFFFFF',
+        'whitesmoke': '#F5F5F5',
+        'yellow': '#FFFF00',
+        'yellowgreen': '#9ACD32'}
+
+    # temperature API
+    api_key = "c2b907d7afa19047e69979b4e75494af"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city_name = "Toronto"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    current_temperature = None
+    current_pressure = None
+    current_humidity = None
+    weather_description = None
+    if x["cod"] != "404":
+        y = x["main"]
+        current_temperature = y["temp"]
+        current_pressure = y["pressure"]
+        current_humidity = y["humidity"]
+        z = x["weather"]
+        weather_description = z[0]["description"]
+    else:
+        print(" City Not Found ")
+
+    result = []
+    with open("./sentences.txt", "r") as filein:
+        for item in filein:
+            if item.strip() != "":
+                result.append(item.strip())
+
+    today_color = str(random.choice(list(cnames)))
+    today_number = str(random.randint(0, 1000))
+    today_sentence = str(random.choice(list(result)))
+    current_temperature = str(round(current_temperature - 273.15, 3))
+    current_pressure = str(current_pressure)
+    current_humidity = str(current_humidity)
+    weather_description = str(weather_description)
+    current = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+
+    # sentence_4 = f"Good Morning, Mr. Han.\n\nToday is {current}.\nTemperature is {current_temperature}.\nAtmospheric " \
+    #              f"pressure is {current_pressure}.\nHumidity is {current_humidity}.\nDescription is {weather_description}.\n\n" \
+    #              f"Today share: {today_sentence}\n\nHave a good day, Sir Dechen Han."
+
+    sentence_4 = f"Good Morning, Mr. Wei.\n\nToday is {current}.\nToday lucky number is {today_number}.\nToday " \
+                 f"lucky colour is {today_color}.\n\nTemperature is {current_temperature}.\nAtmospheric " \
+                 f"pressure is {current_pressure}.\nHumidity is {current_humidity}.\nDescription is {weather_description}.\n\n" \
+                 f"Today share: {today_sentence}\n\nHave a good day, Sir Ertong Wei."
+    message = [sentence_4]
+
+    global global_list
+    global_list = message
+    return None
+
+
+def send_message():
+    global global_list
+    subprocess.call("osascript message.applescript '%s' '%s'" %
+                    (f'{phone_number}', f'{random.choice(global_list)}'), shell=True)
+
+
+if __name__ == '__main__':
+    schedule.every().day.at(information_prep_time).do(prepare_message)
+    schedule.every().day.at(scheduled_time).do(send_message)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
